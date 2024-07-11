c638688bb70a07f800e84b93eef1cdb73c8639e4
shauryagupta07 <shauryag049@gmail.com>
2024-07-11 14:44:32 +0530

    updated

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const dbConnect = require('./config/dbConnect.js')
+const dbConnect = require('./backend/config/dbConnect.js')
 const app = express()
 app.use(express.json())
 const path = require('path')
-const userRoute = require('./routes/userRoutes.js')
-const transactionsRoute = require('./routes/transactionRoutes.js')
+const userRoute = require('./backend/routes/userRoutes.js')
+const transactionsRoute = require('./backend/routes/transactionRoutes.js')
 app.use('/api/users/' , userRoute)
 app.use('/api/transactions/' , transactionsRoute)
 
